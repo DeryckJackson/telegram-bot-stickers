19d6e1b1ba20fa2baca9b3499105f1dc08233ba6
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2021-03-29 18:33:01 -0700

    webpack updates

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import $ from "jquery";
+import sharp from "sharp";
+
+$(document).ready(() => {
+
+});
+
+// const resizeHeight = (input) => {
+//   sharp(input)
+//     .resize({ height: 512 })
+//     .toFile('output.png', (err) => {
+//       console.log(err);
+//     });
+// };
+
+// const resizeWidth = (input) => {
+//   sharp(input)
+//     .resize({ width: 512 })
+//     .toFile('output.png', (err) => {
+//       console.log(err);
+//     });
+// };
