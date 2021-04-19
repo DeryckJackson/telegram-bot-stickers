ad10232a82020ea1e468e5892f2da9a9393fa84a
Jerax <dgjackon01@gmail.com>
2021-04-18 17:34:20 -0700

    add input for sticker pack name and function for getting pack

--- src/index.js
+++ src/index.js
@@ -1,47 +1,62 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
-import $ from 'jquery';
-import axios from 'axios';
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles.css'
+import $ from 'jquery'
+import axios from 'axios'
 
 axios.defaults.baseURL = 'http://localhost:3000';
 
 $(document).ready(() => {
   $('#image').change((event) => {
-    previewImage(event);
+    previewImage(event)
   });
   $('#submit').click(() => {
-    postPhotoData();
+    postPhotoData()
   });
+  $('#test').click(() => {
+    testGet()
+  })
+  $('#getStickerButton').click(() => {
+    getStickerPack($('#stickerName').val())
+  })
 });
+
+async function getStickerPack(name) {
+  try {
+    const response = await axios.get(`/stickers/${name}`)
+    console.log(response.data)
+  } catch (err) {
+    console.log(err)
+  }
+}
 
 async function postPhotoData() {
   const photo = document.getElementById('image').files[0];
-  const formData = new FormData();
+  const formData = new FormData()
 
-  formData.append('photo', photo);
+  formData.append('photo', photo)
 
   try {
-    axios.post('/', formData);
+    axios.post('/', formData)
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
 }
 
 async function testGet() {
   try {
-    const response = await axios.get('/');
-    console.log(response);
+    const response = await axios.get('/')
+    console.log(response.data)
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
 }
 
 function previewImage(event) {
-  const reader = new FileReader();
+  const reader = new FileReader()
   reader.onload = () => {
-    const output = document.getElementById('output-image');
-    output.src = reader.result;
+    const output = document.getElementById('output-image')
+    output.src = reader.result
   };
-  reader.readAsDataURL(event.target.files[0]);
+  reader.readAsDataURL(event.target.files[0])
 }
