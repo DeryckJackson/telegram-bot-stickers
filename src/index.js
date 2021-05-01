2110e703ce5c67354b49329a76188577c6106394
Jerax <dgjackon01@gmail.com>
2021-05-01 13:12:21 -0700

    add emoji input and ability to append emojis to it

--- src/index.js
+++ src/index.js
@@ -3,22 +3,42 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles.css'
 import $ from 'jquery'
 import axios from 'axios'
+import { EmojiButton } from '@joeattardi/emoji-button'
 
 axios.defaults.baseURL = 'http://localhost:3000';
 
 $(document).ready(() => {
+  const picker = new EmojiButton({
+    position: 'bottom-start'
+  })
+  const trigger = document.querySelector('#trigger')
+
   $('#image').change((event) => {
     previewImage(event)
-  });
+  })
+
   $('#submit').click(() => {
     postPhotoData()
-  });
+  })
+
   $('#test').click(() => {
     testGet()
   })
+
   $('#getStickerButton').click(() => {
     getStickerPack($('#stickerName').val())
   })
+
+  $(trigger).click(() => {
+    picker.togglePicker(trigger)
+  })
+
+  picker.on('emoji', selection => {
+    $('#emoji').val((i, val) => {
+      return val + selection.emoji
+    })
+  })
+
 });
 
 async function getStickerPack(name) {
